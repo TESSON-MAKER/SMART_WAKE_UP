1906ef4bb62b8611944133e518fc26767322a5cf
TESSON-MAKER <85193015+TESSON-MAKER@users.noreply.github.com>
2023-12-01 16:13:08 +0100

    Update gpio.h

--- SMART_WAKE_UP/gpio.h
+++ SMART_WAKE_UP/gpio.h
@@ -3,7 +3,6 @@
 
 #include "stm32f7xx.h"
 
-void enableGPIOClock(GPIO_TypeDef *GPIO);
 void GPIO_PinMode(GPIO_TypeDef *GPIO, uint32_t PIN, uint8_t mode);
 void GPIO_DigitalWrite(GPIO_TypeDef *PORT, uint16_t PIN, uint8_t state);
 uint8_t GPIO_DigitalRead(GPIO_TypeDef *PORT, uint16_t PIN);

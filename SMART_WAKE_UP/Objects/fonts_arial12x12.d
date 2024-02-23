a854cdcf8ec7b6e745e0143508712fd2161b964d
TESSON-MAKER <85193015+TESSON-MAKER@users.noreply.github.com>
2024-02-23 15:36:04 +0100

    Delete SMART_WAKE_UP/Objects directory

--- SMART_WAKE_UP/Objects/fonts_arial12x12.d
+++
@@ -1,2 +0,0 @@
-./objects/fonts_arial12x12.o: fonts_arial12x12.c \
-  D:\SOFT\uVision\Keil_V5\ARM\ARMCLANG\Bin\..\include\stdint.h fonts.h
--- SMART_WAKE_UP/Objects/fonts_arial28x28.d
+++
@@ -1,2 +0,0 @@
-./objects/fonts_arial28x28.o: fonts_arial28x28.c \
-  D:\SOFT\uVision\Keil_V5\ARM\ARMCLANG\Bin\..\include\stdint.h fonts.h

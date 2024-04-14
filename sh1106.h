758f10ba36ae21b17c5382d373987c6ed0f24f10
TESSON-MAKER <paul.tesson.officiel@gmail.com>
2024-04-14 14:43:09 +0200

    Update commands for SH1106

--- sh1106.h
+++ sh1106.h
@@ -9,39 +9,50 @@
 #include "fonts.h"
 
 // Screen dimensions
-#define SH1106_WIDTH (uint8_t)128 
-#define SH1106_HEIGHT (uint8_t)64
-#define SH1106_DATA_SIZE (uint8_t)8
+#define SH1106_WIDTH (uint8_t) 128 
+#define SH1106_HEIGHT (uint8_t) 64
+#define SH1106_DATA_SIZE (uint8_t) 8
 
-// SH1106 command definitions
-#define SH1106_CMD_SETMUX    (uint8_t)0xA8 // Set multiplex ratio (N, number of lines active on display)
-#define SH1106_CMD_SETOFFS   (uint8_t)0xD3 // Set display offset
-#define SH1106_CMD_STARTLINE (uint8_t)0x40 // Set display start line
-#define SH1106_CMD_SEG_NORM  (uint8_t)0xA0 // Column 0 is mapped to SEG0 (X coordinate normal)
-#define SH1106_CMD_SEG_INV   (uint8_t)0xA1 // Column 127 is mapped to SEG0 (X coordinate inverted)
-#define SH1106_CMD_COM_NORM  (uint8_t)0xC0 // Scan from COM0 to COM[N-1] (N - mux ratio, Y coordinate normal)
-#define SH1106_CMD_COM_INV   (uint8_t)0xC8 // Scan from COM[N-1] to COM0 (N - mux ratio, Y coordinate inverted)
-#define SH1106_CMD_COM_HW    (uint8_t)0xDA // Set COM pins hardware configuration
-#define SH1106_CMD_CONTRAST  (uint8_t)0x81 // Contrast control
-#define SH1106_CMD_EDON      (uint8_t)0xA5 // Entire display ON enabled (all pixels on, RAM content ignored)
-#define SH1106_CMD_EDOFF     (uint8_t)0xA4 // Entire display ON disabled (output follows RAM content)
-#define SH1106_CMD_INV_OFF   (uint8_t)0xA6 // Entire display inversion OFF (normal display)
-#define SH1106_CMD_INV_ON    (uint8_t)0xA7 // Entire display inversion ON (all pixels inverted)
-#define SH1106_CMD_CLOCKDIV  (uint8_t)0xD5 // Set display clock divide ratio/oscillator frequency
-#define SH1106_CMD_DISP_ON   (uint8_t)0xAF // Display ON
-#define SH1106_CMD_DISP_OFF  (uint8_t)0xAE // Display OFF (sleep mode)
+// SH1106 command definitions 
+#define SH1106_CMD_COL_LOW      (uint8_t) 0x00 // Set Lower Column Address
+#define SH1106_CMD_COL_HIGH     (uint8_t) 0x10 // Set Higher Column Address
+#define SH1106_CMD_STARTLINE    (uint8_t) 0x40 // Set Display Start Line
+#define SH1106_CMD_CONTRAST     (uint8_t) 0x81 // Set Contrast Control Register (Double Bytes Command)
+#define SH1106_CMD_SEG_NORM     (uint8_t) 0xA0 // Set Segment Re-map (X coordinate normal)
+#define SH1106_CMD_SEG_INV      (uint8_t) 0xA1 // Set Segment Re-map (X coordinate inverted)
+#define SH1106_CMD_EDON         (uint8_t) 0xA5 // Set Entire Display OFF/ON (Right rotates)
+#define SH1106_CMD_EDOFF        (uint8_t) 0xA4 // Set Entire Display OFF/ON (Left rotates)
+#define SH1106_CMD_INV_OFF      (uint8_t) 0xA6 // Set Normal/Reverse Display (pixels in normal display)
+#define SH1106_CMD_INV_ON       (uint8_t) 0xA7 // Set Normal/Reverse Display (all pixels inverted)
+#define SH1106_CMD_SETMUX       (uint8_t) 0xA8 // Set Multiplex Ration (Double Bytes Command)
+#define SH1106_CMD_DCDC         (uint8_t) 0xAD // Set DC-DC OFF/ON (Double Bytes Command)
+#define SH1106_CMD_DCDC_ON      (uint8_t) 0x8B // Set DC-DC ON
+#define SH1106_CMD_DCDC_OFF     (uint8_t) 0x8A // Set DC-DC OFF
+#define SH1106_CMD_DISP_ON      (uint8_t) 0xAF // Set Display ON
+#define SH1106_CMD_DISP_OFF     (uint8_t) 0xAE // Set Display OFF (sleep mode)
+#define SH1106_CMD_PAGE_ADDR    (uint8_t) 0xB0 // Set Page Address
+#define SH1106_CMD_COM_NORM     (uint8_t) 0xC0 // Set Common Output Scan Direction (Y coordinate normal)
+#define SH1106_CMD_COM_INV      (uint8_t) 0xC8 // Set Common Output Scan Direction (Y coordinate inverted)
+#define SH1106_CMD_SETOFFS      (uint8_t) 0xD3 // Set display offset (Double Bytes Command)
+#define SH1106_CMD_CLOCKDIV     (uint8_t) 0xD5 // Set display clock divide ratio/oscillator frequency (Double Bytes Command)
+#define SH1106_CMD_DPCHARGE_PER (uint8_t) 0xD9 // Set Dis-charge/Pre-charge Period (Double Bytes Command)
+#define SH1106_CMD_COM_HW       (uint8_t) 0xDA // Set COM pins hardware configuration (Double Bytes Command)
+#define SH1106_CMD_VCOM_DLEVEL  (uint8_t) 0xDB // Set VCOM Deselect Level (Double Bytes Command)
 
-#define SH1106_CMD_COL_LOW   (uint8_t)0x00 // Set Lower Column Address
-#define SH1106_CMD_COL_HIGH  (uint8_t)0x10 // Set Higher Column Address
-#define SH1106_CMD_PAGE_ADDR (uint8_t)0xB0 // Set Page Address
+#define SH1106_CMD_RMWRITE      (uint8_t) 0xE0 // Read-Modify-Write
+#define SH1106_CMD_END          (uint8_t) 0xEE // End
+#define SH1106_CMD_NOP          (uint8_t) 0xE3 // Nop
 
-#define SH1106_CMD_CHARGE    (uint8_t)0x22 //  Dis-charge / Pre-charge Period
-#define SH1106_CMD_SCRL_HR   (uint8_t)0x26 // Setup continuous horizontal scroll right
-#define SH1106_CMD_SCRL_HL   (uint8_t)0x27 // Setup continuous horizontal scroll left
-#define SH1106_CMD_SCRL_VHR  (uint8_t)0x29 // Setup continuous vertical and horizontal scroll right
-#define SH1106_CMD_SCRL_VHL  (uint8_t)0x2A // Setup continuous vertical and horizontal scroll left
-#define SH1106_CMD_SCRL_STOP (uint8_t)0x2E // Deactivate scroll
-#define SH1106_CMD_SCRL_ACT  (uint8_t)0x2F // Activate scroll
+
+
+
+#define SH1106_CMD_CHARGE    (uint8_t) 0x22 //  Dis-charge / Pre-charge Period
+#define SH1106_CMD_SCRL_HR   (uint8_t) 0x26 // Setup continuous horizontal scroll right
+#define SH1106_CMD_SCRL_HL   (uint8_t) 0x27 // Setup continuous horizontal scroll left
+#define SH1106_CMD_SCRL_VHR  (uint8_t) 0x29 // Setup continuous vertical and horizontal scroll right
+#define SH1106_CMD_SCRL_VHL  (uint8_t) 0x2A // Setup continuous vertical and horizontal scroll left
+#define SH1106_CMD_SCRL_STOP (uint8_t) 0x2E // Deactivate scroll
+#define SH1106_CMD_SCRL_ACT  (uint8_t) 0x2F // Activate scroll
 
 void SH1106_Init(void);
 void SH1106_SetPixel(uint8_t pixel, int16_t x, int16_t y);

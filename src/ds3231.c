8be3d539cb5e07c4934e6a427588a60b3ff7caad
TESSON-MAKER <paul.tesson.officiel@gmail.com>
2024-10-05 20:57:20 +0200

    Update ds3231.c

--- src/ds3231.c
+++ src/ds3231.c
@@ -9,25 +9,25 @@
 ********************************************************************/
 void DS3231_Init(void) 
 {
-    RCC->AHB1ENR |= RCC_AHB1ENR_GPIOBEN;
-    RCC->APB1ENR |= RCC_APB1ENR_I2C1EN;
-    
-    GPIOB->MODER |= GPIO_MODER_MODER8_1;
-    GPIOB->MODER &= ~GPIO_MODER_MODER8_0;
-    
-    GPIOB->MODER |= GPIO_MODER_MODER9_1;
-    GPIOB->MODER &= ~GPIO_MODER_MODER9_0;
-    
-    GPIOB->OTYPER |= GPIO_OTYPER_OT8;
-    GPIOB->OTYPER |= GPIO_OTYPER_OT9;
+	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOBEN;
+	RCC->APB1ENR |= RCC_APB1ENR_I2C1EN;
 
-    GPIOB->OSPEEDR |= GPIO_OSPEEDER_OSPEEDR8;
-    GPIOB->OSPEEDR |= GPIO_OSPEEDER_OSPEEDR9;
-    
-    GPIOB->AFR[1] |= (AF4<<0)|(AF4<<4);
+	GPIOB->MODER |= GPIO_MODER_MODER8_1;
+	GPIOB->MODER &= ~GPIO_MODER_MODER8_0;
 
-    I2C1->CR1 &= ~I2C_CR1_PE;
-    I2C1->TIMINGR = 0x0000C1C1;
+	GPIOB->MODER |= GPIO_MODER_MODER9_1;
+	GPIOB->MODER &= ~GPIO_MODER_MODER9_0;
+
+	GPIOB->OTYPER |= GPIO_OTYPER_OT8;
+	GPIOB->OTYPER |= GPIO_OTYPER_OT9;
+
+	GPIOB->OSPEEDR |= GPIO_OSPEEDER_OSPEEDR8;
+	GPIOB->OSPEEDR |= GPIO_OSPEEDER_OSPEEDR9;
+
+	GPIOB->AFR[1] |= (AF4<<0)|(AF4<<4);
+
+	I2C1->CR1 &= ~I2C_CR1_PE;
+	I2C1->TIMINGR = 0x0000C1C1;
 }
 
 /*******************************************************************
@@ -39,7 +39,7 @@ void DS3231_Init(void)
 ********************************************************************/
 int DS3231_BCD_DEC(unsigned char x) 
 {
-    return x - 6 * (x >> 4); // Conversion of BCD to decimal
+	return x - 6 * (x >> 4); // Conversion of BCD to decimal
 }
 
 /*******************************************************************
@@ -51,7 +51,7 @@ int DS3231_BCD_DEC(unsigned char x)
 ********************************************************************/
 int DS3231_DEC_BCD(unsigned char x) 
 {
-    return x + 6 * (x / 10); // Conversion of decimal to BCD
+	return x + 6 * (x / 10); // Conversion of decimal to BCD
 }
 
 /*******************************************************************
@@ -72,7 +72,7 @@ void DS3231_Read(uint8_t slav_add, uint8_t memadd, uint8_t *data, uint8_t length
 	I2C1->CR2 |= I2C_CR2_START; // Generate start
 
 	while(!(I2C1->ISR & I2C_ISR_TC)) // Wait until transfer is completed
-    {
+	{
 		if(I2C1->ISR & I2C_ISR_TXE) // Check if TX buffer is empty
 		{
 			I2C1->TXDR = (memadd); // Send memory address

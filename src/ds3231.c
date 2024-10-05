06548e176a1503dec2e3b797cf78a735c5822ef4
TESSON-MAKER <paul.tesson.officiel@gmail.com>
2024-10-05 20:55:11 +0200

    Minor update

--- src/ds3231.c
+++ src/ds3231.c
@@ -1,5 +1,12 @@
 #include "../inc/ds3231.h"
 
+/*******************************************************************
+ * @name       :DS3231_Init
+ * @date       :2024-01-03
+ * @function   :Initializes the DS3231 by configuring I2C and GPIO
+ * @parameters :None
+ * @retvalue   :None
+********************************************************************/
 void DS3231_Init(void) 
 {
     RCC->AHB1ENR |= RCC_AHB1ENR_GPIOBEN;
@@ -23,17 +30,38 @@ void DS3231_Init(void)
     I2C1->TIMINGR = 0x0000C1C1;
 }
 
+/*******************************************************************
+ * @name       :DS3231_BCD_DEC
+ * @date       :2024-01-03
+ * @function   :Converts BCD (Binary Coded Decimal) to Decimal
+ * @parameters :x (unsigned char) - BCD value to be converted
+ * @retvalue   :int - Converted decimal value
+********************************************************************/
 int DS3231_BCD_DEC(unsigned char x) 
 {
-    return x - 6 * (x >> 4); // Conversion of BCD in decimal
+    return x - 6 * (x >> 4); // Conversion of BCD to decimal
 }
 
+/*******************************************************************
+ * @name       :DS3231_DEC_BCD
+ * @date       :2024-01-03
+ * @function   :Converts Decimal to BCD (Binary Coded Decimal)
+ * @parameters :x (unsigned char) - Decimal value to be converted
+ * @retvalue   :int - Converted BCD value
+********************************************************************/
 int DS3231_DEC_BCD(unsigned char x) 
 {
-    return x + 6 * (x / 10); // Conversion of decimal in BCD
+    return x + 6 * (x / 10); // Conversion of decimal to BCD
 }
 
-void DS3231_Read(uint8_t slav_add, uint8_t memadd, uint8_t *data, uint8_t length ) 
+/*******************************************************************
+ * @name       :DS3231_Read
+ * @date       :2024-01-03
+ * @function   :Reads data from the DS3231 device over I2C
+ * @parameters :slav_add, memadd, data, length
+ * @retvalue   :None
+********************************************************************/
+void DS3231_Read(uint8_t slav_add, uint8_t memadd, uint8_t *data, uint8_t length) 
 {
 	I2C1->CR1 |= I2C_CR1_PE; // Enable I2C
 	I2C1->CR2 = (slav_add<<1); // Set slave address
@@ -61,7 +89,7 @@ void DS3231_Read(uint8_t slav_add, uint8_t memadd, uint8_t *data, uint8_t length
 
 	while(!(I2C1->ISR & I2C_ISR_STOPF)) // Wait until stop is generated
 	{
-		if(I2C1->ISR & I2C_ISR_RXNE) // If RX buffer is empty
+		if(I2C1->ISR & I2C_ISR_RXNE) // If RX buffer is not empty
 		{
 			*data++ = I2C1->RXDR; // Read the data and increment the pointer
 		}
@@ -70,6 +98,13 @@ void DS3231_Read(uint8_t slav_add, uint8_t memadd, uint8_t *data, uint8_t length
 	I2C1->CR1 &= ~I2C_CR1_PE; // Disable the peripheral
 }
 
+/*******************************************************************
+ * @name       :DS3231_WriteMemory
+ * @date       :2024-01-03
+ * @function   :Writes data to the DS3231 device over I2C
+ * @parameters :slav_add, memadd, data, length
+ * @retvalue   :None
+********************************************************************/ 
 void DS3231_WriteMemory(uint8_t slav_add, uint8_t memadd, uint8_t *data, uint8_t length) 
 {
 	I2C1->CR1 |= I2C_CR1_PE; // Enable I2C
@@ -80,12 +115,11 @@ void DS3231_WriteMemory(uint8_t slav_add, uint8_t memadd, uint8_t *data, uint8_t
 	I2C1->CR2 |= I2C_CR2_AUTOEND; // Hardware end
 	I2C1->CR2 |= I2C_CR2_START; // Generate start
 
-	int i = 0;
 	while (!(I2C1->ISR & I2C_ISR_STOPF)) // Wait until stop is generated
 	{
 		if (I2C1->ISR & I2C_ISR_TXE) // Check if TX buffer is empty
 		{
-			I2C1->TXDR = *data++; // Send memory address
+			I2C1->TXDR = *data++; // Send memory address and data
 		}
 	}
 

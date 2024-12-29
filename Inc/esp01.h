0654ae8070445ac65a7c71b3dacae458dcabe012
TESSON-MAKER <paul.tesson.officiel@gmail.com>
2024-12-29 19:38:29 +0100

    New funtions in esp01

--- Inc/esp01.h
+++ Inc/esp01.h
@@ -8,12 +8,20 @@
 #define ESP01_BAUDRATE 115200
 #define UART7_AF8 0x08
 
-// UART Initialization
+#define MAX_RESPONSE_SIZE 128
+#define DEFAULT_TIMEOUT_MS 5000
+#define MAX_RETRIES 3
+#define API_KEY "test"
+#define OPENWEATHER_HOST "test"
+
 void ESP01_Usart_Init(void);
 void ESP01_Send(const char *command);
 int ESP01_Receive(char *buffer, int bufferSize, int timeoutMs);
 int ESP01_SendAndCheck(const char *command, const char *expected, int timeoutMs);
 int ESP01_InitWiFi(const char *ssid, const char *password);
 int ESP01_GetTime(char *timeBuffer, int bufferSize, int timeoutMs);
+void ESP01_GetWeather(const char *city, char *responseBuffer, int bufferSize);
+float ESP01_ParseTemperature(const char *response);
+float ESP01_GetTemperature(const char *city);
 
 #endif /* ESP01_H */
--- Src/esp01.c
+++ Src/esp01.c
@@ -1,11 +1,8 @@
 #include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include "esp01.h"
 #include "tim.h"
-
-#define MAX_RESPONSE_SIZE 128
-#define DEFAULT_TIMEOUT_MS 5000
-#define MAX_RETRIES 3
 
 /*******************************************************************
  * @name       :ESP01_Usart_Init
@@ -22,19 +19,19 @@ void ESP01_Usart_Init(void)
     RCC->APB1ENR |= RCC_APB1ENR_UART7EN;
 
     // Configure PE8 in alternate function mode (UART7 TX)
-    GPIOE->MODER &= ~GPIO_MODER_MODER8;     // Clear the configuration bits
-    GPIOE->MODER |= GPIO_MODER_MODER8_1;    // Set PE8 to alternate function mode
+    GPIOE->MODER &= ~GPIO_MODER_MODER8;
+    GPIOE->MODER |= GPIO_MODER_MODER8_1;
 
     // Configure PE7 in alternate function mode (UART7 RX)
-    GPIOE->MODER &= ~GPIO_MODER_MODER7;     // Clear the configuration bits
-    GPIOE->MODER |= GPIO_MODER_MODER7_1;    // Set PE7 to alternate function mode
+    GPIOE->MODER &= ~GPIO_MODER_MODER7;
+    GPIOE->MODER |= GPIO_MODER_MODER7_1;
 
     // Assign alternate function AF7 to PE8 (UART7 TX)
-    GPIOE->AFR[1] &= ~(UART7_AF8 << GPIO_AFRH_AFRH0_Pos); // Clear existing bits
+    GPIOE->AFR[1] &= ~(UART7_AF8 << GPIO_AFRH_AFRH0_Pos);
     GPIOE->AFR[1] |= (UART7_AF8 << GPIO_AFRH_AFRH0_Pos);
 
     // Assign alternate function AF7 to PE7 (UART7 RX)
-    GPIOE->AFR[0] &= ~(UART7_AF8 << GPIO_AFRL_AFRL7_Pos); // Clear existing bits
+    GPIOE->AFR[0] &= ~(UART7_AF8 << GPIO_AFRL_AFRL7_Pos);
     GPIOE->AFR[0] |= (UART7_AF8 << GPIO_AFRL_AFRL7_Pos);
 
     // Set the baud rate
@@ -76,31 +73,31 @@ int ESP01_Receive(char *buffer, int bufferSize, int timeoutMs)
 {
     int i = 0;
     int elapsedTime = 0;
-    const int tickInterval = 1; // 1ms per loop iteration
 
     while (i < bufferSize - 1)
     {
-        // Wait for a character to be received or timeout
+        // Wait for a character or timeout
         while (!(UART7->ISR & USART_ISR_RXNE))
         {
             if (elapsedTime >= timeoutMs)
             {
-                buffer[i] = '\0'; // Null-terminate in case of timeout
+                buffer[i] = '\0'; // Null-terminate the string
                 return 0;         // Timeout occurred
             }
-            elapsedTime += tickInterval;
-            TIM_Wait(tickInterval); // Wait for 1ms
+            elapsedTime++;
+            TIM_Wait(1); // Wait for 1 ms
         }
 
-        // Read the received character
+        // Read and append the received character
         char c = UART7->RDR;
         buffer[i++] = c;
 
-        // Stop at the end of the line
+        // Stop if end of line is reached
         if (c == '\n') break;
     }
+
     buffer[i] = '\0'; // Null-terminate the string
-    return 1;         // Successfully received data
+    return 1;         // Data successfully received
 }
 
 /*******************************************************************
@@ -113,22 +110,17 @@ int ESP01_Receive(char *buffer, int bufferSize, int timeoutMs)
 ********************************************************************/
 int ESP01_SendAndCheck(const char *command, const char *expected, int timeoutMs)
 {
-    int retries = 0;
-    while (retries < MAX_RETRIES)
+    for (int retries = 0; retries < MAX_RETRIES; retries++)
     {
         ESP01_Send(command); // Send the command
         char response[MAX_RESPONSE_SIZE];
 
-        if (ESP01_Receive(response, sizeof(response), timeoutMs))
+        if (ESP01_Receive(response, sizeof(response), timeoutMs) && strstr(response, expected))
         {
-            if (strstr(response, expected) != NULL)
-            {
-                return 1; // Expected response received
-            }
+            return 1; // Expected response received
         }
-        retries++;
     }
-    return 0; // Max retries reached or response not received
+    return 0; // Max retries reached or no valid response
 }
 
 /*******************************************************************
@@ -140,15 +132,12 @@ int ESP01_SendAndCheck(const char *command, const char *expected, int timeoutMs)
 ********************************************************************/
 int ESP01_InitWiFi(const char *ssid, const char *password)
 {
-    // Set ESP01 to station mode and connect to Wi-Fi
     if (!ESP01_SendAndCheck("AT+CWMODE=1\r\n", "OK", 2000))
         return 0;
 
-    // Create Wi-Fi connect command
     char command[MAX_RESPONSE_SIZE];
     snprintf(command, sizeof(command), "AT+CWJAP=\"%s\",\"%s\"\r\n", ssid, password);
 
-    // Send the command and check the response
     return ESP01_SendAndCheck(command, "OK", 5000);
 }
 
@@ -162,14 +151,72 @@ int ESP01_InitWiFi(const char *ssid, const char *password)
 ********************************************************************/
 int ESP01_GetTime(char *timeBuffer, int bufferSize, int timeoutMs)
 {
-    // Configure the NTP server
-    ESP01_Send("AT+CIPSNTPCFG=1,0,\"pool.ntp.org\"\r\n");
+    ESP01_Send("AT+CIPSNTPCFG=1,1,\"pool.ntp.org\"\r\n");
 
-    // Request the NTP time
-    if (!ESP01_SendAndCheck("AT+CIPSNTPTIME?\r\n", "+CIPSNTPTIME:", timeoutMs))
-        return 0;
-
-    // Receive the time
-    return ESP01_Receive(timeBuffer, bufferSize, timeoutMs);
+    return ESP01_SendAndCheck("AT+CIPSNTPTIME?\r\n", "+CIPSNTPTIME:", timeoutMs) &&
+           ESP01_Receive(timeBuffer, bufferSize, timeoutMs);
 }
+
+/*******************************************************************
+ * @name       :ESP01_GetWeather
+ * @function   :Send HTTP GET request to OpenWeatherMap for weather data
+ * @parameters :city - Name of the city
+ *              responseBuffer - Buffer to store HTTP response
+ *              bufferSize - Size of the buffer
+ * @retvalue   :None
+********************************************************************/
+void ESP01_GetWeather(const char *city, char *responseBuffer, int bufferSize)
+{
+    ESP01_Send("AT+CIPSTART=\"TCP\",\"" OPENWEATHER_HOST "\",80\r\n");
+    if (!ESP01_SendAndCheck("", "OK", 5000))
+    {
+        return;
+    }
+
+    char request[256];
+    snprintf(request, sizeof(request),
+             "GET /data/2.5/weather?q=%s&appid=%s&units=metric HTTP/1.1\r\n"
+             "Host: %s\r\n"
+             "Connection: close\r\n\r\n",
+             city, API_KEY, OPENWEATHER_HOST);
+
+    char command[512];
+    snprintf(command, sizeof(command), "AT+CIPSEND=%d\r\n", strlen(request));
+    ESP01_Send(command);
+    ESP01_Receive(responseBuffer, bufferSize, 5000);
+    ESP01_Send("AT+CIPCLOSE\r\n");
+}
+
+/*******************************************************************
+ * @name       :ESP01_ParseTemperature
+ * @function   :Parse temperature from JSON response
+ * @parameters :response - HTTP response containing JSON data
+ * @retvalue   :Temperature as a float, or -999.0 on error
+********************************************************************/
+float ESP01_ParseTemperature(const char *response)
+{
+    const char *tempPtr = strstr(response, "\"temp\":");
+    if (tempPtr != NULL)
+    {
+        return atof(tempPtr + 7);
+    }
+    return -999.0;
+}
+
+/*******************************************************************
+ * @name       :ESP01_GetTemperature
+ * @function   :Get temperature of a city using OpenWeatherMap API
+ * @parameters :city - Name of the city
+ * @retvalue   :Temperature as a float, or -999.0 on error
+********************************************************************/
+float ESP01_GetTemperature(const char *city)
+{
+    char response[512];
+    memset(response, 0, sizeof(response));
+    ESP01_GetWeather(city, response, sizeof(response));
+    return ESP01_ParseTemperature(response);
+}
+
+
+
 
